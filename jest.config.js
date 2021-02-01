c306686d045d9fb5c4555329d8c78482adbdca0e
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-01 14:32:48 -0300

    Refactor structure folder

--- jest.config.js
+++ jest.config.js
@@ -26,10 +26,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 40,
-      branches: 40,
-      functions: 40,
-      lines: 40,
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
     },
   },
 }
