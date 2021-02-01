ff311cebd32347f43e4e0f20eb023baa01b55810
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-01 15:09:32 -0300

    Added truthiness helpers

--- src/sum.test.js
+++ src/sum.test.js
@@ -28,4 +28,15 @@ test('null', () => {
     expect(n).toBeFalsy();
 });
 
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
+
+
+
 //test('', () => {});
