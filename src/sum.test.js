8dfcd4623e7df10804b50b4eb26ae4484ad9a2b6
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-01 14:56:19 -0300

    Added matches toEqual

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,12 @@
+const { test, expect } = require('@jest/globals');
 const suma = require('./sum')
 
 test('should return 3 when adds two numbers 1 and 2', () => {
     expect(suma(1,2)).toBe(3)
+});
+
+test('object assignment', () => {
+    const data = {one:1}
+    data['two'] = 2
+    expect(data).toEqual({one:1, two:2})
 });
