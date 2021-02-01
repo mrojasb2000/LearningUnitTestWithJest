0935c7d1443372bc3be7cdb6ef207d62f443ad3e
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-01 15:30:58 -0300

    Added numbers test

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,3 +1,4 @@
+const { expect } = require('@jest/globals');
 const suma = require('./sum')
 
 test('should return 3 when adds two numbers 1 and 2', () => {
@@ -40,6 +41,21 @@ describe("Truthiness", () => {
     });
 });
 
+describe("Numbes", () => {
+    it('two plus two', () => {
+        const value = 2 + 2;
+        expect(value).toBeGreaterThan(3);
+        expect(value).toBeGreaterThanOrEqual(3.5);
+        expect(value).toBeLessThan(5);
+        expect(value).toBeLessThanOrEqual(4.5);
 
+        // toBe and toEqual are equivalent for numbers
+        expect(value).toBe(4);
+        expect(value).toEqual(4);
+    });
+}); 
 
 //test('', () => {});
+/* describe("Truthiness", () => {
+    it('null', () => {});
+});  */   
