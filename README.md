c51f31a80dac626561cdf05931688d215154f1f7
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-01 15:49:06 -0300

    Added strings tests

--- README.md
+++ README.md
@@ -73,3 +73,13 @@ test('', () => {
     expect(value).toBeCloseTo(0.3)
 });
 ```
+
+## Strings
+```sh
+test('there is no I in team', () => {
+   expect('team').not.toMatch(/I/);
+});
+test('but there is a "stop" in Christoph', () => {
+  expect('Christoph').toMatch(/stop/)
+});
+```
--- src/sum.test.js
+++ src/sum.test.js
@@ -59,6 +59,14 @@ describe("Numbes", () => {
     });
 }); 
 
+describe("Strings", () => {
+    it('there is no I in team', () => {
+        expect('team').not.toMatch(/I/);
+    });
+    it('but there is a "stop" in Christoph', () => {
+        expect('Christoph').toMatch(/stop/)
+    });
+}); 
 
 
 //test('', () => {});
