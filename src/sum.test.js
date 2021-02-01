6024245ec7d042f62e3ce3b5ad750d37dbdcdb5b
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-01 14:59:08 -0300

    Added matchers toBe

--- src/sum.test.js
+++ src/sum.test.js
@@ -2,11 +2,19 @@ const { test, expect } = require('@jest/globals');
 const suma = require('./sum')
 
 test('should return 3 when adds two numbers 1 and 2', () => {
-    expect(suma(1,2)).toBe(3)
+    expect(suma(1, 2)).toBe(3)
 });
 
 test('object assignment', () => {
-    const data = {one:1}
+    const data = { one: 1 }
     data['two'] = 2
-    expect(data).toEqual({one:1, two:2})
+    expect(data).toEqual({ one: 1, two: 2 })
+});
+
+test('adding positive numbers is to zero', () => {
+    for (let a = 1; a < 10; a++) {
+        for (let b = 1; b < 10; b++) {
+            expect(a + b).not.toBe(0)
+        }
+    }
 });
